1615780ad9b962344e75bee5d538101011caa7cd
root <cleoquintiliano@gmail.com>
2014-11-27 03:33:09 -0200

    Gravando

--- lojaecommerce/urls.py
+++ lojaecommerce/urls.py
@@ -5,6 +5,7 @@ from django.conf.urls import patterns, include, url
 # admin.autodiscover()
 
 urlpatterns = patterns('',
+    (r'^catalogo/$', 'preview.views.home'),         
     # Examples:
     # url(r'^$', 'lojaecommerce.views.home', name='home'),
     # url(r'^lojaecommerce/', include('lojaecommerce.foo.urls')),
